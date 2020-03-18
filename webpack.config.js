1d0cc4e63f63e3e0124d12958e119a0b45a66176
zhoucumt <553817478@qq.com>
2020-03-18 17:24:18 +0800

    add webpack.config.js and .babelrc

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//webpack.config.js
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/ //排除 node_modules 目录
+            }
+        ]
+    }
+}
