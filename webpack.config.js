f854534ff90c533e1d66f279b7a40c17f3edecfb
zhoucumt <553817478@qq.com>
2020-03-18 17:51:50 +0800

    use HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 //webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
+
     module: {
         rules: [
             {
@@ -22,5 +25,18 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+
+    plugins: [
+        //数组 放着所有的webpack插件
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html', //打包后的文件名
+            minify: {
+                removeAttributeQuotes: false, //是否删除属性的双引号
+                collapseWhitespace: false, //是否折叠空白
+            },
+            // hash: true //是否加上hash，默认是 false
+        })
+    ]
 }
