6b42971219164e89a576e9dcaeb1df7b5136d04a
zhoucumt <553817478@qq.com>
2020-03-19 09:48:31 +0800

    add devServer in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -41,5 +41,15 @@ module.exports = {
             config: config.template
             // hash: true //是否加上hash，默认是 false
         })
-    ]
+    ],
+
+    devServer: {
+        port: '3000', //默认是8080
+        quiet: false, //默认不启用
+        inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
+        stats: "errors-only", //终端仅打印 error
+        overlay: false, //默认不启用
+        clientLogLevel: "silent", //日志等级
+        compress: true //是否启用 gzip 压缩
+    }
 }
