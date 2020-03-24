95ec91a8e6b3bec502abba29a6ecadcb4c80122b
zhoucumt <553817478@qq.com>
2020-03-24 21:06:08 +0800

    按需加载

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 //src/index.js
 import './index.less';
 
-class Animal {
-    constructor(name) {
-        this.name = name;
-    }
-    getName() {
-        return this.name;
-    }
-}
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     getName() {
+//         return this.name;
+//     }
+// }
 
-const dog = new Animal('dog');
+// const dog = new Animal('dog');
 console.log('aaaa');
+
+document.getElementById('btn').onclick = function() {
+    import('./handle.js').then(fn => fn.default());
+}
